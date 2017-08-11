919725273c4ca32eea714049f089ca5e5910f2ff
Andrew Tenajeros <jules.tenajeros@gmail.com>
2017-08-11 13:01:20 +0800

    change

--- views/js/script.js
+++ views/js/script.js
@@ -2,8 +2,8 @@
 $(function(){
     var sys_show_popup_login = $(".sys_show_popup_login"),
         sys_popup_common = $("#sys_popup_common");
-	/* Open popup when click to: Register, Login
-	---------------------------------------------------------- */
+    /* Open popup when click to: Register, Login
+    ---------------------------------------------------------- */
     sys_show_popup_login.on("click",function(){
         sys_popup_common.fadeIn();
         $("body").on("keydown.closePopup",function(e){
@@ -23,3 +23,58 @@ $(function(){
         e.stopPropagation();
     });
 });
+
+let user = [{ //array of objects !!!!
+        
+
+    }];
+
+        function createUser(){
+
+            let name = document.querySelector('#txt_name').value;
+            let email = document.querySelector('#txt_email').value;
+            let password = document.querySelector('#txt_password').value;
+            let newAccount = {
+                name: name,
+                email: email,
+                password: password
+            };
+    user.push(newAccount);
+
+    console.log(user);
+    let jns = JSON.stringify(user);
+    localStorage.setItem('user',jns);
+
+    let jns_retrieve = localStorage.getItem('user');
+    console.log(jns_retrieve);
+
+    let jns_parse = JSON.parse(localStorage.getItem('user'));
+    console.log(jns_parse);
+
+    //Display in console inside of json array
+    console.log("Name is " + jns_parse[0].name);
+    console.log("Email is " + jns_parse[0].email)
+    console.log("password is " + jns_parse[0].password)
+    
+        };
+            function readuser(){
+                    
+                
+                console.log(user);
+                let jns = JSON.stringify(user);
+                localStorage.setItem('user',jns);
+
+                let jns_retrieve = localStorage.getItem('user');
+                console.log(jns_retrieve);
+
+                let jns_parse = JSON.parse(localStorage.getItem('user'));
+                console.log(jns_parse);
+                console.log(user.length);
+                    var initialLength = user.length;
+                    for(var i=0; i<initialLength; ++i) {
+                        
+                console.log("Name: " + jns_parse[i].name);
+                console.log("Email: " + jns_parse[i].email);
+                    }
+            };
+
